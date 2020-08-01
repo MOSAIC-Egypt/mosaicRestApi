242230004105bbcce03c40cd414174e951af86a0
minamagdy01 <mina_magdy01@outlook.com>
2020-08-01 18:33:41 +0200

    make it better

---
+++ configs/mongoconfig.js
@@ -0,0 +1,6 @@
+
+//use your secret
+module.exports={
+    mongoConfig:"mongodb+srv://mon:mon12345678@cluster0.dp1dy.mongodb.net/mosaic?retryWrites=true&w=majority"
+  }
+  
--- keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOSTNAME_DB: process.env.HOSTNAME_DB,
-    PORT_DB: process.env.PORT_DB,
-    USER_DB: process.env.USER_DB,
-    PASSWORD_DB: process.env.PASSWORD_DB,
-    NAME_DB: process.env.NAME_DB,
-}
